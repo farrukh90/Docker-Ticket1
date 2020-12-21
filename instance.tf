a3e9322143ec485a3d1191f6cda0fe954d4da7ff
farrukh90 <farrukhsadykov@gmail.com>
2020-12-21 23:02:32 +0000

    removed public IP

--- instance.tf
+++ instance.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "Test" {
   ami                         = "${var.ami_id}"
-  associate_public_ip_address = true
   instance_type               = "${var.instance_type}"
   vpc_security_group_ids      = ["${aws_security_group.ticket1.id}"]
   key_name                    = "${aws_key_pair.Ticket1.key_name}"
